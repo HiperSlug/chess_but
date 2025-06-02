659ab029cfe8cfa1dc75ff9fe528ecac81646e92
Raphael <raphael.larsen@proton.me>
2025-06-02 16:20:14 -0400

    Added health_check workaround.

---
+++ builds/server/health_check_workaround.py
@@ -0,0 +1,15 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+def run():
+    server = HTTPServer(('0.0.0.0', 80), Handler)
+    print("Health check server running on port 80")
+    server.serve_forever()
+
+if __name__ == "__main__":
+    run()
